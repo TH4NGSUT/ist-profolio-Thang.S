4c4fa52c96c90bdae03b6dec1f39de368a1eb458
TH4NGSUT <2027sutsang.thang@fcboe.org>
2024-01-24 15:11:10 -0500

    Hello

---
+++ c/scanf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int age;
+	float gpa;
+	char grade;
+
+	scanf("how old are you: %i\n", &age);
+	scanf("what is your gpa: %f\n", &gpa);
+	scanf("what grade are you in: %c\n", &grade);
+
+	return 0;
+}
